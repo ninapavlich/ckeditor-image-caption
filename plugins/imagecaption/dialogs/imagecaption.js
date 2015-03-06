18f6d2a9f75d49abe8a3de48b197aab48950c2f2
Nina Pavlich <nina@ninalp.com>
2015-03-06 14:01:59 -0500

    bugfix for images without figures

--- plugins/imagecaption/dialogs/imagecaption.js
+++ plugins/imagecaption/dialogs/imagecaption.js
@@ -331,7 +331,9 @@
 					this.originalElement.setCustomData( 'isReady', 'false' );
 
 
+
 					if( figure ){
+						
 						this.figureElement = figure;
 						this.figureEditMode = true;
 
@@ -393,7 +395,22 @@
 						this.imageElement = element;
 					}
 
+					if(!this.figureElement){
+						//If there wasn't a figure before, then let's create a figure wrapper for the image
+						this.figureElement = editor.document.createElement( 'figure' );
+						this.figureCaptionElement = editor.document.createElement( 'figcaption' );
 
+						if(this.imageElement){
+							this.figureElement.append(this.imageElement)
+							this.figureElement.append(this.figureCaptionElement);
+							//Insert a new Figure.
+							editor.insertElement( this.figureElement );
+						}
+						
+					}
+					
+
+					
 					
 					if ( this.imageEditMode ) {
 						// Use the original element as a buffer from  since we don't want
@@ -992,7 +1009,8 @@
 									items: [
 										[ editor.lang.common.alignLeft, 'left' ],
 										[ editor.lang.common.alignRight, 'right' ],
-										[ editor.lang.common.alignCenter, 'center' ]
+										[ editor.lang.common.alignCenter, 'center' ],
+										[ 'None', 'none' ]
 										// Backward compatible with v2 on setup when specified as attribute value,
 										// while these values are no more available as select options.
 										//	[ editor.lang.image.alignAbsBottom , 'absBottom'],
